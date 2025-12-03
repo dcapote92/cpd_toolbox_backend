6f1e86a2865c3f7ee9932a7a8ff437f7e786c10f
dcapote92 <danicapote92@gmail.com>
2025-12-03 12:12:50 -0300

    onrender added to allowebd host

--- app/settings.py
+++ app/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-4k=fpy+g$mg6i%r4d@6=lmavw5lfi$i1y!=qguu2@@94)dtj2-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'cpd-toolbox.onrender.com',
+]
 
 
 # Application definition
