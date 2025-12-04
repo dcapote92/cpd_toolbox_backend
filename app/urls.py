2ee372363884b3ca4c6cd5e64ff661ea0311f921
dcapote92 <danicapote92@gmail.com>
2025-12-04 15:21:47 -0300

    api url/resources versioning added

--- app/urls.py
+++ app/urls.py
@@ -1,18 +1,10 @@
 from django.contrib import admin
-from django.urls import path
-from models.views import ModelCreateListView, ModelRetrieveUpdateDestroy
-from sections.views import SectionCreateListView, SectionRetrieveUpdateDestroy
-from scales.views import ScaleCreateListView, ScaleRetrieveUpdateDestroy
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     
-    path('models/', ModelCreateListView.as_view(), name='model-list-view'),
-    path('models/<int:pk>/', ModelRetrieveUpdateDestroy.as_view(), name='model-detail-view'),
-
-    path('sections/', SectionCreateListView.as_view(), name='section-list-view'),
-    path('sections/<int:pk>/', SectionRetrieveUpdateDestroy.as_view(), name='section-detail-view'),
-
-    path('scales/', ScaleCreateListView.as_view(), name='scale-list-view'),
-    path('scales/<int:pk>/', ScaleRetrieveUpdateDestroy.as_view(), name='scale-detail-view'),
+    path('api/v1', include('models.urls')),
+    path('api/v1', include('scales.urls')),
+    path('api/v1', include('sections.urls'))
 ]
