ca3a33353bc90013d3681a9ce1a9cb7c12ab5f14
dcapote92 <danicapote92@gmail.com>
2025-12-11 21:54:41 -0300

    minor fixes

--- sections/models.py
+++ sections/models.py
@@ -1,8 +1,23 @@
 from django.db import models
 
 
+SECTION_CHOICES = (
+    ('FRL', 'Frente de Loja'),
+    ('ACO', 'Açougue'),
+    ('PEI', 'Peixaria'),
+    ('FRI', 'Frios e Laticinios'),
+    ('PAD', 'Padaria'),
+    ('HOR', 'Hortifrúti'),
+    ('DOC', 'Docas Secas'),
+    ('DOCF', 'Docas Frias')
+)
+
+
 class Section(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(
+        max_length=100,
+        choices=SECTION_CHOICES,
+    )
 
     def __str__(self):
         return self.name
