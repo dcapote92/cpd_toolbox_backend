b160da65b504c6080558811d9be83b945ebc0b1c
dcapote92 <danicapote92@gmail.com>
2025-12-11 22:00:08 -0300

    authenticate app installed

--- app/urls.py
+++ app/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
 
+    path('api/v1/', include('authenticate.urls')),
     path('api/v1/', include('models.urls')),
     path('api/v1/', include('scales.urls')),
     path('api/v1/', include('sections.urls'))
